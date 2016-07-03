2b054dc0c19c2aff1a17fc2b303fb3f114f015be
Max Resch <max.resch@student.tuwien.ac.at>
2016-07-03 18:44:51 +0200

    portability improvements

--- build.rs
+++ build.rs
@@ -11,25 +11,28 @@ use std::io::Write;
 static CPLEX_DEFAULT: &'static str = "/opt/ibm/ILOG/CPLEX_Studio126";
 
 #[cfg(target_os = "linux")]
+static CPLEX_SUBPATH : &'static str = "cplex/bin/x86-64_linux";
+
+#[cfg(target_os = "linux")]
 static CPLEX_LIBRARY_NAME: &'static str = "libcplex*.so";
 
-fn get_cplex_library_name(cpxpath: &str) -> PathBuf {
-	for entry in glob((String::from(cpxpath) + "/" + CPLEX_LIBRARY_NAME).as_str()).unwrap() {
+fn get_cplex_library_name(cpxpath: &PathBuf) -> PathBuf {
+	for entry in glob(cpxpath.join(CPLEX_LIBRARY_NAME).to_str().unwrap()).unwrap() {
 		return entry.unwrap().clone();
 	}
 	unreachable!()
 }
 
 fn main() {
-	let cpxpath = env::var("CPLEX_DIR").unwrap_or(String::from(CPLEX_DEFAULT));
-    let cpxpath = cpxpath + "/cplex/bin/x86-64_linux";
+	let cpxpath = PathBuf::from(env::var("CPLEX_DIR").unwrap_or(String::from(CPLEX_DEFAULT)));
+    let cpxpath = cpxpath.join(CPLEX_SUBPATH);
 	let library = get_cplex_library_name(&cpxpath);
 	(writeln!(&mut std::io::stderr(), "Found CPLEX library at: {}", library.display())).unwrap();
 
 	let libname = String::from(library.file_stem().unwrap().to_str().unwrap());
 	let libname = libname.trim_left_matches("lib");
 
-    println!("cargo:rustc-link-search=native={}", cpxpath);
+    println!("cargo:rustc-link-search=native={}", cpxpath.to_str().unwrap());
     println!("cargo:rustc-link-lib={}", libname);
 
     let target = PathBuf::from(env::var("OUT_DIR").unwrap()).join(library.file_stem().unwrap());
