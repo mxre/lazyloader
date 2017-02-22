bb5af6bfa7cd7eff45c2780333382df0b5af8253
Max Resch <max.resch@student.tuwien.ac.at>
2017-02-22 06:00:18 +0100

    windows: link to universal C runtime

--- example/nmake.mk
+++ example/nmake.mk
@@ -2,7 +2,7 @@
 INCLUDES = -I"$(CPLEX_STUDIO_DIR1263)/cplex/include" -I../include
 LIBS = -LIBPATH:..\lib cplex.lib
 
-CFLAGS = -MD -Ox -D_WIN32_WINNT=0x0601 -DBUILD_CPXSTATIC=1
+CFLAGS = -MT -Ox -D_WIN32_WINNT=0x0601 -DBUILD_CPXSTATIC=1
 LDFLAGS = -subsystem:console -nodefaultlib:libucrt.lib ucrt.lib
 
 .PHONY: all
@@ -13,7 +13,7 @@ example.obj: example.c
 	$(CC) $(INCLUDES) $(CFLAGS) -c example.c
 
 example.exe: example.obj
-	link example.obj $(LIBS) -out:$@
+	link $(LDFLAGS) example.obj $(LIBS) -out:$@
 
 clean:
 	del /F example.exe example.obj
